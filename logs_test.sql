b3ebef159021dd7f19db49c19482788912203b45
Philippe Girard <philippe.girard5@gmail.com>
2018-05-11 16:10:48 -0400

    add phil user

--- logs_test.sql
+++ logs_test.sql
@@ -1,3 +1,6 @@
 SELECT truncate_all_tables('POSTGRES');
 
-INSERT INTO membres()
+INSERT INTO membres(cip, faculteID, departementID, nom, prenom) VALUES
+ ('girp2705', 18, 08, 'girard', 'philippe');
+ 
+ DELETE from membres where cip = 'girp2705';
