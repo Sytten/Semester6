8c4467d5aa9bdeca9c03a9eaefde944c4f98fef9
Philippe Girard <philippe.girard5@gmail.com>
2018-05-11 15:17:45 -0400

    default

--- createDB.sql
+++ createDB.sql
@@ -176,8 +176,8 @@ EQUIPEMENTID
 create table CATEGORIES (
    CATEGORIEID          INT4                 not null,
    NOM                  VARCHAR(1024)        not null,
-   BLOCDEBUT            INT4                 not null,
-   BLOCFIN              INT4                 not null,
+   BLOCDEBUT            INT4                 not null     DEFAULT 0,
+   BLOCFIN              INT4                 not null     DEFAULT 95,
    constraint PK_CATEGORIES primary key (CATEGORIEID)
 );
 
