0df7847a30395674c0a9bf488839c59ea4a99235
Philippe Girard <philippe.girard5@gmail.com>
2018-05-12 17:28:52 -0400

    hello

--- Creation/tableau.sql
+++ Creation/tableau.sql
@@ -1,5 +1,9 @@
-SELECT reservations.date as date, locauxTimeList.timeBloc, locauxTimeList.numeropavillon, locauxTimeList.numerolocal, locauxTimeList.categorieId, reservations.evenementid as evenementid FROM
+SELECT reservations.date as date, locauxTimeList.timeBloc as dayTime,
+ (locauxTimeList.numeropavillon || '-' || locauxTimeList.numerolocal) as local,
+ locauxTimeList.categorieId, reservations.evenementid as evenementid, e.nom FROM
  (SELECT * FROM possibleTimeBloc CROSS JOIN (SELECT numeropavillon, numerolocal, categorieid FROM locaux) AS locauxNames) AS locauxTimeList
  LEFT JOIN reservations ON locauxTimeList.timeBloc = reservations.numerobloc AND locauxTimeList.numeropavillon = reservations.numeropavillon AND locauxTimeList.numerolocal = reservations.numerolocal
-WHERE evenementid IS NOT NULL -- TODO remove
+ LEFT JOIN evenements e on reservations.evenementid = e.evenementid -- ajout nom d'evenement
 ORDER BY date, timeBloc;
+
+-- WHERE evenementid IS NOT NULL -- TODO remove
