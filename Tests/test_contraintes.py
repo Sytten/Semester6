5d31a98feff7e4c5b960338c8f5de51e42a28d59
Philippe Girard <philippe.girard5@gmail.com>
2018-05-14 22:20:28 -0400

    domain on everything else

--- Tests/test_contraintes.py
+++ Tests/test_contraintes.py
@@ -19,3 +19,29 @@ class TestContraintes(TestBase):
 
     def test_categorie_bloc_between_0_and_95(self):
         self.db.execute("""INSERT INTO categories VALUES(9999, 'nom de local', 0, 95)""")
+
+    def test_bloc_possible_out_0_95(self):
+        self.db.execute("""TRUNCATE TABLE blocTempsPossible""")
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+            self.db.execute("""INSERT INTO bloctempspossible VALUES(-1, 'ninenine')""")
+
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+            self.db.execute("""INSERT INTO bloctempspossible VALUES(96, 'sixxxsix')""")
+
+    def test_bloc_possible_between_0_95(self):
+        self.db.execute("""TRUNCATE TABLE blocTempsPossible""")
+        self.db.execute("""INSERT INTO bloctempspossible VALUES(0, 'zero')""")
+        self.db.execute("""INSERT INTO bloctempspossible VALUES(95, 'ninetofive')""")
+
+    def test_reservation_bloc_out_0_95(self):
+        self.db.execute("""INSERT INTO evenements VALUES (0,'hi')""")
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+            self.db.execute("""INSERT INTO reservations VALUES ('D7',3020,'2200-04-05',-1,0,'girp2705')""")
+
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+            self.db.execute("""INSERT INTO reservations VALUES ('D7',3020,'2200-04-05',96,0,'girp2705')""")
+
+    def test_reservation_bloc_between_0_95(self):
+        self.db.execute("""INSERT INTO evenements VALUES (0,'hi')""")
+        self.db.execute("""INSERT INTO reservations VALUES ('D7',3020,'2200-04-05',0,0,'girp2705')""")
+        self.db.execute("""INSERT INTO reservations VALUES ('D7',3020,'2200-04-05',95,0,'girp2705')""")
