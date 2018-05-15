ac63186cdd400f48638050d714b5b3bef61fab10
Philippe Girard <philippe.girard5@gmail.com>
2018-05-14 22:48:56 -0400

    fix tests

--- Tests/test_contraintes.py
+++ Tests/test_contraintes.py
@@ -5,26 +5,26 @@ from test_base import TestBase
 
 class TestContraintes(TestBase):
     def test_categorie_bloc_out_0_and_95(self):
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"categories\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO categories VALUES(9999, 'nom de local', 0, 96)""")
 
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"categories\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO categories VALUES(9999, 'nom de local', 0, -1)""")
 
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"categories\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO categories VALUES(9999, 'nom de local', -1, 0)""")
 
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"categories\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO categories VALUES(9999, 'nom de local', 96, 0)""")
 
     def test_categorie_bloc_between_0_and_95(self):
         self.db.execute("""INSERT INTO categories VALUES(9999, 'nom de local', 0, 95)""")
 
     def test_bloc_possible_out_0_95(self):
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"bloctempspossible\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO bloctempspossible VALUES(-1, 'ninenine')""")
 
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"bloctempspossible\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO bloctempspossible VALUES(96, 'sixxxsix')""")
 
     def test_bloc_possible_between_0_95(self):
@@ -33,10 +33,10 @@ class TestContraintes(TestBase):
 
     def test_reservation_bloc_out_0_95(self):
         self.db.execute("""INSERT INTO evenements VALUES (0,'hi')""")
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"reservations\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO reservations VALUES ('D7',3020,'2200-04-05',-1,0,'girp2705')""")
 
-        with self.assertRaisesRegex(psycopg2.IntegrityError, "value for domain blocdomain violates check constraint \"blocdomain_check\""):
+        with self.assertRaisesRegex(psycopg2.IntegrityError, "new row for relation \"reservations\" violates check constraint \"bloc_domain\""):
             self.db.execute("""INSERT INTO reservations VALUES ('D7',3020,'2200-04-05',96,0,'girp2705')""")
 
     def test_reservation_bloc_between_0_95(self):
